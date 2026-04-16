20d5b335d35f15e8b754193f1d1b81d8a8d42247
rahulthamballa895 <rahulthamballa895@gmail.com>
2026-04-16 14:52:02 +0530

    Add unit test for calculateFine method

--- Library Management System/src/test/java/com/example/AppTest.java
+++ Library Management System/src/test/java/com/example/AppTest.java
@@ -1,1 +1,13 @@
+package com.example;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testFine() {
+        App app = new App();
+        assertEquals(30, app.calculateFine(10));
+    }
+}
