9fc0f57d03980e62e24c1d0f8ac156a21f6248ac
rahulthamballa895 <rahulthamballa895@gmail.com>
2026-04-16 14:51:13 +0530

    Update App.java

--- Library Management System/src/main/java/com/example/App.java
+++ Library Management System/src/main/java/com/example/App.java
@@ -1,1 +1,13 @@
+package com.example;
 
+public class App {
+
+    public int calculateFine(int days) {
+        int allowed = 7;
+
+        if (days > allowed) {
+            return (days - allowed) * 10;
+        }
+        return 0;
+    }
+}
